f21051e675b6d1ffc0aed4e5d4843c4c4edf2bb4
kftang <kennyftang@gmail.com>
2021-03-06 18:16:08 -0600

    Updated to use bundling

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socketserver
+import socket
 import struct
 import math
 import uuid
@@ -24,26 +25,21 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
       global count, last_time, still_count, current_log
       print('connected to new host')
       while True:
+        bundled_data = []
+
         # self.request is the TCP socket connected to the client
-        self.data.extend(list(self.request.recv(128)))
-        while len(self.data) >= 60:
-          end_index = 0
-          while struct.unpack_from('I', bytes(self.data), offset=end_index)[0] != 0xDEADBEEF:
-            end_index += 1
-            if end_index == len(self.data) - 4:
-              break
-          if end_index < 56:
-            print(f'discarding {end_index} bytes from partial struct')
-            del self.data[0:end_index + 4]
-            break
-          print(f'found data struct ending at byte offset {end_index}')
-          if end_index > 56:
-            print(f'removing first {end_index - 56} bytes')
-            del self.data[0:end_index - 56]
-          
-          data = bytes(self.data[0:60])
-          del self.data[0:60]
-          
+        data = self.request.recv(4)
+        find_start = struct.unpack_from('I', data)[0]
+
+        while find_start != 0x00000000:
+          data = self.request.recv(4)
+          if len(data) != 4:
+            continue
+          find_start = struct.unpack_from('I', data)[0]
+
+        print('found data bundle')
+        for _ in range(50):
+          data = self.request.recv(56, socket.MSG_WAITALL)
           parsed_data = {
           'accel': list(struct.unpack_from('fff', data)),
           'gyro': list(struct.unpack_from('fff', data, offset=12)),
@@ -52,34 +48,40 @@ class MyTCPHandler(socketserver.BaseRequestHandler):
           'pitch': struct.unpack_from('f', data, offset=40)[0],
           'roll': struct.unpack_from('f', data, offset=44)[0],
           'time': struct.unpack_from('Q', data, offset=48)[0],
-          'verify': struct.unpack_from('I', data, offset=56)[0],
           }
-          last_time = parsed_data['time']
-          accel_magnitude = math.sqrt(sum(map(lambda accel_val : accel_val ** 2, parsed_data['accel'])))
-          parsed_data['accelMagnitude'] = accel_magnitude
+          bundled_data.append(parsed_data)
 
-          if abs(accel_magnitude - 1) < .1:
-            if still_count > 4 and len(current_log) > 20:
-              fname = uuid.uuid4()
-              fd = open(f'data/{fname}.txt', 'w')
-              fd.write(json.dumps(current_log))
-              fd.close()
-              print(f'Writing {len(current_log)} data points to {fname}.txt')
-              current_log.clear()
-            still_count += 1
-            if still_count < 4:
-              current_log.append(parsed_data)
-          else:
-            if still_count > 4:
-              current_log.clear()
-            still_count = 0
-            current_log.append(parsed_data)
+        data = self.request.recv(4)
+        find_end = struct.unpack_from('I', data)[0]
+        if find_end != 0xFFFFFFFF:
+          print('data bundle was corrupted')
+          bundled_data.clear()
 
-        count += 1
-        if count % 10 == 0:
-          # print(f'delta-t: {(parsed_data["time"] - last_time) / 1000}')
-          # print("{} wrote {}".format(self.client_address[0], parsed_data))
-          pass
+        print('found end of data bundle')
+
+        if len(bundled_data):
+          current_log = []
+          for data in bundled_data:
+            accel_magnitude = math.sqrt(sum(map(lambda accel_val : accel_val ** 2, data['accel'])))
+            data['accelMagnitude'] = accel_magnitude
+
+            if abs(accel_magnitude - 1) < .1:
+              if still_count > 4 and len(current_log) > 20:
+                fname = uuid.uuid4()
+                fd = open(f'data/{fname}.txt', 'w')
+                fd.write(json.dumps(current_log))
+                fd.close()
+                print(f'Writing {len(current_log)} data points to {fname}.txt')
+                current_log.clear()
+
+              still_count += 1
+              if still_count < 4:
+                current_log.append(data)
+            else:
+              if still_count > 4:
+                current_log.clear()
+              still_count = 0
+              current_log.append(data)
 
 
 if __name__ == '__main__':
